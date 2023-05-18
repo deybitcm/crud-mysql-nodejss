c6074be86d56292c24979a9d44e67eb71846da74
David <deybitcm@unjbg.edu.pe>
2023-05-18 17:36:25 -0500

    Configurando remote db

--- src/db.js
+++ src/db.js
@@ -1,9 +1,14 @@
 import { createPool } from "mysql2/promise";
+import * as fs from "node:fs";
 
 export const pool = createPool({
-  host: "localhost",
-  user: "root",
-  password: "123456",
+  host: 'web-server-prueba.mysql.database.azure.com',
+  user: 'David',
+  password: 'S3naqu3rib',
   port: 3306,
-  database: "customersdb",
+  database: 'customersdb',
+  ssl: { 
+    rejectUnauthorized: true,
+    ca:fs.readFileSync("ssl/DigiCertGlobalRootCA.crt.pem","utf8")
+  } 
 });
