72bd4a277059b07706602a933759ef06d6f0d5b7
David <deybitcm@unjbg.edu.pe>
2023-05-18 16:56:26 -0500

    cambio dde variable para db

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ import { createPool } from "mysql2/promise";
 export const pool = createPool({
   host: "localhost",
   user: "root",
-  password: "faztpassword",
+  password: "123456",
   port: 3306,
   database: "customersdb",
 });
