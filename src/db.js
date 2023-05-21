c05156e26833c154ff4b43f7c3b671cf673d174a
David <deybitcm@unjbg.edu.pe>
2023-05-20 23:34:41 -0500

    Cambio de variable puerto

--- src/db.js
+++ src/db.js
@@ -7,12 +7,13 @@ const host = process.env.DB_HOST;
 const db = process.env.DB_NAME;
 const user = process.env.DB_USER;
 const password = process.env.DB_PASSWORD;
+const puerto = process.env.DB_PORT;
 
 export const pool = createPool({
   host: host,
   user: user,
   password: password,
-  port: 3306,
+  port: puerto,
   database: db,
   ssl: { 
     rejectUnauthorized: true,
