2596236f44e3cdf541fd9c55e9001e42f0202042
David <deybitcm@unjbg.edu.pe>
2023-05-20 23:22:49 -0500

    Cambio de reglas de contraseña cifrada

--- src/db.js
+++ src/db.js
@@ -1,12 +1,19 @@
 import { createPool } from "mysql2/promise";
 import * as fs from "node:fs";
+import { config } from 'dotenv';
+config();
+
+const host = process.env.DB_HOST;
+const db = process.env.DB_NAME;
+const user = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
 
 export const pool = createPool({
-  host: 'web-server-prueba.mysql.database.azure.com',
-  user: 'David',
-  password: 'S3naqu3rib',
+  host: host,
+  user: user,
+  password: password,
   port: 3306,
-  database: 'customersdb',
+  database: db,
   ssl: { 
     rejectUnauthorized: true,
     ca:fs.readFileSync("ssl/DigiCertGlobalRootCA.crt.pem","utf8")
